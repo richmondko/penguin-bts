34a6ff39b5c76e2edec604ecfdd1a317c43adb33
Richmond Jerald C. Ko <richmond_ko@hotmail.com>
2015-03-21 22:00:45 +0800

    change footer

---
+++ app/admin/footer.rb
@@ -0,0 +1,16 @@
+module ActiveAdmin
+  module Views
+    class Footer < Component
+
+      def build
+        super :id => "footer"                                                    
+        super :style => "text-align: left;"                                     
+
+        div do                                                                   
+          "Penguin BTS by PenguinLabs #{Date.today.year}"                                       
+        end
+      end
+
+    end
+  end
+end
